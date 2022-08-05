548d599ad78f770aa2e552300a6ea278f5559493
Adrián Alejandro Cervantes Amezcua <42442780+AdrianCervantes98@users.noreply.github.com>
2022-08-05 17:51:17 -0500

    Add files via upload

--- simuladorInteractivoChallenge/js/login_script.js
+++ simuladorInteractivoChallenge/js/login_script.js
@@ -1,3 +1,5 @@
+var shoppingCart = [];
+
 function loginFunction() {
     //User info for login purposes
     const username = 'adrian.cervantes';
@@ -29,6 +31,45 @@ function login(user, password, inputUser, inputPassword) {
     } else {
         return false;
     }
+}
+
+function addToCart() {
+    let inputProductName = prompt('Please enter the product name');
+    let inputQty = parseInt(prompt('Please input the product quantity'));
+    let newItem = {
+        name: inputProductName,
+        qty: inputQty
+    };
+    if(inputQty < 1 || isNaN(inputQty)) {
+        alert('Quantity is less than 1 or not a number');
+    } else {
+        let flag = false;
+        if(shoppingCart.length > 0) {
+            for(let i = 0; i < shoppingCart.length; i++) {
+                if(shoppingCart[i].name == inputProductName) {
+                    flag = true;
+                    shoppingCart[i].qty += inputQty;
+                }
+            }
+        }
+        
+        if(!flag) {
+            shoppingCart.push(newItem);
+        }
+    }
+}
+
+function clearCart() {
+    shoppingCart.length = 0;
+}
+
+function showCart() {
+    if(shoppingCart.length > 0) {
+        alert(JSON.stringify(shoppingCart));
+    } else {
+        alert('Empty cart.')
+    }
+    
 }
 
 
